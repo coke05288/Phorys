196ba9efe7eb53df6a660327180a3b4b5ef071a3
YoungJun <coke05288@gmail.com>
2024-10-06 17:32:25 +0900

    git init

---
+++ src/main/java/com/phorys/phorys/PhorysApplication.java
@@ -0,0 +1,13 @@
+package com.phorys.phorys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhorysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhorysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/phorys/phorys/PhorysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phorys.phorys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhorysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
